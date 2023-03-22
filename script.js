56ef674193df83b41606bbe2c838c38839ddb240
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-22 19:44:39 -0400

    dynamic hitbox shifting when window re-sizes

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // width of screen
-    const SCREENWIDTH = document.documentElement.clientWidth;
-    const CLICKERWIDTH = SCREENWIDTH / 1.75 * 0.1115;
-    const CLICKERMARGIN = SCREENWIDTH / 179.2
+    // fixes hitboxes for whenever the window is resized
+    resizeHitboxes(); window.addEventListener('resize', resizeHitboxes);
 
+    // detects clicks on hitboxes
     Array.from(document.getElementsByClassName("clicker")).forEach((element, index) => {
-
-        // adjusts left position so they are not right on top of each other
-        element.style.left = String(index * CLICKERWIDTH + CLICKERMARGIN) + "px";
 
         element.addEventListener("click", function() {
             console.log(index + "clicked")
             // TODO: do something with the index to designate which one is being clicked
         })
     })
+    
 });
+
+// changes hitboxes when screen resized
+function resizeHitboxes() {
+
+    var screenWidth = document.documentElement.clientWidth;
+    var clickerWidth = screenWidth / 1.75 * 0.1115;
+    var clickerMargin = screenWidth / 179.2;
+
+    Array.from(document.getElementsByClassName("clicker")).forEach((element, index) => {
+
+        // adjusts left position so they are not right on top of each other
+        element.style.left = String(index * clickerWidth + clickerMargin) + "px";
+
+    })
+}
