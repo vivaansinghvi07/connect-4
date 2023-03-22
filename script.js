8e8c751d8a8e1136d3b408faf640e2388de80a4e
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-22 19:05:45 -0400

    added script

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll(".clicks").forEach((element, index) => {
+        element.addEventListener("click", function() {
+            // TODO: do something with the index to designate which one is being clicked
+        })
+    })
+});
