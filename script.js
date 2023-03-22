47917322a996d70625159b7a92b43e691d7a0320
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-22 19:29:33 -0400

    finished display logic

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll(".clicks").forEach((element, index) => {
+    // width of screen
+    const SCREENWIDTH = document.documentElement.clientWidth;
+    const CLICKERWIDTH = SCREENWIDTH / 1.75 * 0.1115;
+    const CLICKERMARGIN = SCREENWIDTH / 179.2
+
+    Array.from(document.getElementsByClassName("clicker")).forEach((element, index) => {
+
+        // adjusts left position so they are not right on top of each other
+        element.style.left = String(index * CLICKERWIDTH + CLICKERMARGIN) + "px";
+
         element.addEventListener("click", function() {
+            console.log(index + "clicked")
             // TODO: do something with the index to designate which one is being clicked
         })
     })
