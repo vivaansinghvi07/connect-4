8bba610a3a8ffe488b9767c2cd9b993a5e469d96
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-22 23:50:14 -0400

    added kbm support

--- script.js
+++ script.js
@@ -8,24 +8,42 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // detects clicks on hitboxes
     Array.from(document.getElementsByClassName("clicker")).forEach((element, index) => {
-
-        element.addEventListener("click", function() {
-
-            // defines height and width
-            let width = document.documentElement.clientWidth;
-            
-            // plays the piece at the index of the click
-            game.placePiece(index);
-            
-            // waits for animation
-            setTimeout(() => {
-                game.display(width);
-                game.checkWin();
-            }, game.time);
-            
-            
+        element.addEventListener("mousedown", function() {
+            play(index);
         })
     });
+
+    // detects keyboard presses
+    document.addEventListener("keypress", function(event) {
+        // defines the possible moves
+        let moves = ['1', '2', '3', '4', '5', '6', '7'];
+
+        // gets the index
+        let index = moves.findIndex((move) => {
+            if (move == event.key) {
+                return true;
+            }
+        });
+
+        // plays if the index is valid
+        if (index !== -1) {
+            play(index);
+        }
+    });
+
+    function play(colNum) {
+        // defines height and width
+        let width = document.documentElement.clientWidth;
+            
+        // plays the piece at the index of the click
+        game.placePiece(colNum);
+        
+        // waits for animation
+        setTimeout(() => {
+            game.display(width);
+            game.checkWin();
+        }, game.time);
+    }
 
     // changes hitboxes when screen resized
     function resizeHitboxes() {
